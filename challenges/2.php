41792d491cab10bad59e3706f72627c4e622ee0e
Anders D. Johnson <adjohnson916@users.noreply.github.com>
2016-11-13 20:13:16 -0600

    fix: 2

--- challenges/2.php
+++ challenges/2.php
@@ -26,10 +26,22 @@ interface AnswerInterface
 
 // start editing here
 
+class Answer implements AnswerInterface {
+  public function get() {
+    return $this;
+  }
+  public function the() {
+    return $this;
+  }
+  public function answer() {
+    return $this;
+  }
+  public function __toString() {
+    return '4';
+  }
+}
 
 // end editing here
 
 $answer = new Answer;
 $question = new Question($answer);
-
-
